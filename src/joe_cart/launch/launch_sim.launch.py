bf85513dc2232f9ec411d3d63865ddddf56dc284
Zawl Ali Fahmy Mohamed <zawlali@outlook.com>
2024-08-12 06:48:55 +0800

    Sprinting

--- src/joe_cart/launch/launch_sim.launch.py
+++ src/joe_cart/launch/launch_sim.launch.py
@@ -68,39 +68,29 @@ def generate_launch_description():
         
     controller_params_file = os.path.join(get_package_share_directory(package_name),'config','my_controllers.yaml')
 
-    controller_manager = Node(
-        package="controller_manager",
-        executable="ros2_control_node",
-        parameters=[controller_params_file],
-        remappings=[
-            ("~/robot_description", "/robot_description"),
-        ],
-    )
-    delayed_controller_manager = TimerAction(period=1.0, actions=[controller_manager])
+    # controller_manager = Node(
+    #     package="controller_manager",
+    #     executable="ros2_control_node",
+    #     parameters=[controller_params_file],
+    #     remappings=[
+    #         ("~/robot_description", "/robot_description"),
+    #     ],
+    # )
+    # delayed_controller_manager = TimerAction(period=1.0, actions=[controller_manager])
 
     diff_drive_spawner = Node(
         package="controller_manager",
         executable="spawner",
         arguments=["diff_cont", "-c", "/controller_manager"],
     )
-    delayed_diff_drive_spawner = RegisterEventHandler(
-        event_handler=OnProcessStart(
-            target_action=controller_manager,
-            on_start=[diff_drive_spawner],
-        )
-    ) 
+
     joint_broad_spawner = Node(
         package="controller_manager",
         executable="spawner",
         arguments=["joint_broad", "-c", "/controller_manager"],
     )
 
-    delayed_joint_broad_spawner = RegisterEventHandler(
-        event_handler=OnProcessStart(
-            target_action=controller_manager,
-            on_start=[joint_broad_spawner],
-        )
-    )
+    \
     # Code for delaying a node (I haven't tested how effective it is)
     # 
     # First add the below lines to imports
@@ -127,6 +117,6 @@ def generate_launch_description():
         twist_mux,
         gazebo,
         spawn_entity,
-        delayed_diff_drive_spawner,
-        delayed_joint_broad_spawner
+        diff_drive_spawner,
+        joint_broad_spawner
     ])
