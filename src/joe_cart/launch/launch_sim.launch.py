826c4b10a2ffd400d2a93a8968f9911365947e3f
Zawl Ali Fahmy Mohamed <zawlali@outlook.com>
2024-08-12 04:45:13 +0800

    Sprinting

--- src/joe_cart/launch/launch_sim.launch.py
+++ src/joe_cart/launch/launch_sim.launch.py
@@ -4,11 +4,12 @@ from ament_index_python.packages import get_package_share_directory
 
 
 from launch import LaunchDescription
-from launch.actions import IncludeLaunchDescription, TimerAction
+from launch.actions import IncludeLaunchDescription, TimerAction, DeclareLaunchArgument
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.actions import RegisterEventHandler
 from launch.event_handlers import OnProcessStart
 from launch_ros.actions import Node
+from launch.substitutions import LaunchConfiguration
 
 
 
@@ -42,11 +43,20 @@ def generate_launch_description():
 
     gazebo_params_file = os.path.join(get_package_share_directory(package_name),'config','gazebo_params.yaml')
 
+    world_file_arg = DeclareLaunchArgument(
+        'world',
+        default_value=os.path.join(get_package_share_directory(package_name), 'worlds', 'joe_mart.world'),
+        description='Full path to the world file to load'
+    )
+
     # Include the Gazebo launch file, provided by the gazebo_ros package
     gazebo = IncludeLaunchDescription(
                 PythonLaunchDescriptionSource([os.path.join(
                     get_package_share_directory('gazebo_ros'), 'launch', 'gazebo.launch.py')]),
-                    launch_arguments={'extra_gazebo_args': '--ros-args --params-file ' + gazebo_params_file}.items()
+                    launch_arguments={
+                        'world': LaunchConfiguration('world'),
+                        'extra_gazebo_args': '--ros-args --params-file ' + gazebo_params_file
+                    }.items()
              )
 
     # Run the spawner node from the gazebo_ros package. The entity name doesn't really matter if you only have a single robot.
@@ -111,6 +121,7 @@ def generate_launch_description():
 
     # Launch them all!
     return LaunchDescription([
+        world_file_arg,
         rsp,
         joystick,
         twist_mux,
